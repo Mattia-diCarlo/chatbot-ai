9e04bde49d5770c95f99c44309aa21bd17d95b42
Mattia di Carlo <mattiadicarlo97@gmail.com>
2026-05-21 21:50:43 +0200

    Fix

--- listmodels.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-API_KEY = "AIzaSyBxNbXv6fBE9ms_4M2ljNpmha3seETkBgw"
-
-url = f"https://generativelanguage.googleapis.com/v1/models?key={API_KEY}"
-
-r = requests.get(url)
-
-print(r.json())
